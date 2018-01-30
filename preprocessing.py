520336a4f66759f6074117032e24a7b783307acf
saminfatehir <saminfatehir@gmail.com>
2018-01-30 14:32:23 -0800

    second file

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,9 @@
 import csv
 import re
 
-with open('C:\Users\user\Documents\university\weetlid-test-tweets.tsv','r') as src:
-    with open('C:\Users\user\Documents\university\data_training.tsv', 'w') as dest:
-        with open('C:\Users\user\Documents\university\data_test.tsv', 'w') as dest2:
+with open('E:\university\weetlid-test-tweets.tsv','r') as src:
+    with open('E:\university\dest.tsv', 'w') as dest:
+        with open('E:\university\dest2.tsv', 'w') as dest2:
             r = csv.reader(src,delimiter='\t')
             w = csv.writer(dest,delimiter='\t')
             w2 = csv.writer(dest2,delimiter='\t')
@@ -14,6 +14,9 @@ with open('C:\Users\user\Documents\university\weetlid-test-tweets.tsv','r') as s
                 text = re.sub("(https://t.co/)\w+", '', text)
                 text = re.sub("(#)\w+", '', text)
                 text = re.sub("(@)\w", '', text)
+                text = re.sub("([#@!$?.,;:*-+/^])", '', text)
+                text = re.sub(r"\d", '', text)
+                text = text.lower()
                 if i > 300:
                     w.writerow([row[0],row[1],row[2],text])
                 else:
